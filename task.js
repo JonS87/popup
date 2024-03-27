02598fc1bb25a259c1edefc497c49d6372d8460b
Evgeniy Chausov <evgeniy@chausov.ru>
2024-03-27 10:13:36 +0300

    C/A HW

--- task.js
+++ task.js
@@ -1,12 +1,23 @@
 const modalClose = document.querySelector('.modal__close');
 const modalCloseParant = document.querySelector('.modal');
-const firstWindow = localStorage.getItem('firstWindow');
+
+function getCookie(name) {
+  const pairs = document.cookie.split('; ');
+  const cookie = pairs.find(p => p.startsWith(name + '='));
+  if (cookie) {
+    return cookie.substr(name.length + 1);
+  } else {
+    return undefined;
+  }
+}
+
+const firstWindow = getCookie('firstWindow');
 
 if (!firstWindow) {
   modalCloseParant.classList.add('modal_active');
 
   modalClose.addEventListener('click', () => {
-    localStorage.setItem('firstWindow', 'close');
+    document.cookie = 'firstWindow=close'; 
     modalCloseParant.classList.remove('modal_active');
   });
 }
